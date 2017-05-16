c110a9e9a40ce5004fefe24699d22adadb50e4df
Julianna <julianna.mikucka@gmail.com>
2017-05-16 19:54:22 +0200

    sd

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,23 +1,26 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  def cache_dir
-    "#{Rails.root}/temp/uploads"
-  end
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-   # include CarrierWave::MiniMagick
+    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-            # storage :file
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-            # def store_dir
-            #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-            # end
+
+ def caches_dir
+   '#{Rails.root}/tmp/uploads'
+ end
+
+#  def store_dir
+#    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+#  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
+  # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
